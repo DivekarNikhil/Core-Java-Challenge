df77aab3e3b77b845584f8df59fe6700a29879f1
DivekarNikhil <nikhildivekar2002@gmail.com>
2024-06-27 19:48:56 +0530

    fifth challeange

--- src/StaticDefaultValue.java
+++ src/StaticDefaultValue.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class StaticDefaultValue {
+public class StaticDefaultValue {
+
+
+
+       static int a;
+       static float b;
+       static double c;
+       static boolean d;
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("int default value :"+a);
+        System.out.println("float default value :"+b);
+        System.out.println("double default value :"+c);
+        System.out.println("boolean default value  "+d);
+
+    }
 }
