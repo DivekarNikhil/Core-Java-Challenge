427f946f795c2575e9f518f2f4282da0f3443c5b
DivekarNikhil <nikhildivekar2002@gmail.com>
2024-06-27 23:43:30 +0530

    string to integer

---
+++ src/StaticAndInstance6.java
@@ -0,0 +1,18 @@
+public class StaticAndInstance6 {
+
+    static  public void sblock()
+    {
+        System.out.println("Static block");
+
+    }
+    {
+        System.out.println("Instance Block");
+    }
+
+    public static void main(String[] args) {
+
+        StaticAndInstance6 st=new StaticAndInstance6();
+        //st.sblock();
+
+    }
+}
---
+++ src/StringToInteger.java
@@ -0,0 +1,8 @@
+public class StringToInteger {
+
+    public static void main(String[] args) {
+        String s="200";
+        int n=Integer.parseInt(s);
+        System.out.println(n+2);
+    }
+}
