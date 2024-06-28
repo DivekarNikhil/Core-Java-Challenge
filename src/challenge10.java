d0e90120745b71abb716d1d133c67e3f46c1b2a9
DivekarNikhil <nikhildivekar2002@gmail.com>
2024-06-28 14:52:38 +0530

    Size of min and max  Value

---
+++ src/challenge10.java
@@ -0,0 +1,8 @@
+public class challenge10 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Byte size"+Byte.SIZE+"Max Value :"+Byte.MAX_VALUE+"Minimum value :"+Byte.MIN_VALUE);
+
+    }
+}
