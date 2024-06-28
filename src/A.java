2d75e7dcd14eafbfb54dfa2644fb0354889b1717
DivekarNikhil <nikhildivekar2002@gmail.com>
2024-06-28 16:21:58 +0530

    Inheritance

---
+++ src/A.java
@@ -0,0 +1,23 @@
+public class A {
+
+    int a=2 ,b=5;
+    int c=a+b;
+    {
+        System.out.println(+c);
+    }
+
+
+}
+
+class  B extends A
+{
+
+public static void main(String[] args)
+    {
+
+        B a=new B();
+        int x=2;
+        System.out.println(a.c);
+
+}
+        }
---
+++ src/SumOfArray11.java
@@ -0,0 +1,19 @@
+public class SumOfArray11 {
+
+    public static void main(String[] args)
+    {
+
+        int a[]={10,20,30,40,50};
+
+        int count=0;
+        for(int element :a)
+        {
+
+            count=count+element;
+            System.out.println(count);
+        }
+
+
+    }
+
+}
